a981e086dfc1464cac329aea7ba46c763f99cb5b
fujiisui <sui_face@yahoo.co.jp>
2025-08-01 21:53:57 +0900

    User認証と初期画面実装：Devise導入／Homeコントローラ設定／ログイン・ログアウト動作確認

---
+++ db/migrate/20250729083013_create_friendships.rb
@@ -0,0 +1,12 @@
+class CreateFriendships < ActiveRecord::Migration[7.1]
+  def change
+    create_table :friendships do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :friend, null: false, foreign_key: { to_table: :users }
+      t.integer :status, null: false, default: 0
+
+      t.timestamps
+    end
+    add_index :friendships, [:user_id, :friend_id], unique: true
+  end
+end
