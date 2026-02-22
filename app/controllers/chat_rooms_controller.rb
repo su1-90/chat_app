f2cc8d527303757a7a74cf3f23484788120bc179
fujiisui <sui_face@yahoo.co.jp>
2026-02-23 07:20:08 +0900

    Refactor ChatRoomsController inputs handling

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -2,8 +2,10 @@ class ChatRoomsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    inputs = index_inputs
+
     @chat_room = ChatRoom.new
-    @chat_rooms = list_chat_rooms
+    @chat_rooms = list_chat_rooms(inputs)
   end
 
   def create
@@ -13,42 +15,52 @@ class ChatRoomsController < ApplicationController
       @chat_room.entries.create!(user: current_user)
       redirect_to @chat_room, flash: { notice: 'チャットルームを作成しました' }
     else
-      @chat_rooms = list_chat_rooms
+      @chat_rooms = list_chat_rooms(index_inputs)
       flash.now[:alert] = 'チャットルームを作成できませんでした'
       render :index, status: :unprocessable_entity
     end
   end
 
   def show
-    @chat_room = ChatRoom.find_by(id: chat_room_id)
+    inputs = show_inputs
+
+    @chat_room = ChatRoom.find_by(id: inputs[:id])
     return redirect_to(chat_rooms_path, alert: 'チャットルームが見つかりません') unless @chat_room
 
     return redirect_to(chat_rooms_path, alert: '権限がありません') unless @chat_room.member?(current_user)
 
-    @messages = @chat_room.messages_for_display(page: page)
+    @messages = @chat_room.messages_for_display(page: inputs[:page])
 
     @message = @chat_room.messages.build(user: current_user)
   end
 
   private
-
-    def chat_room_params
-      params.require(:chat_room).permit(:name)
+    def index_inputs
+      p = params.permit(:page)
+      { page: normalize_page(p[:page]) }
     end
-
-    def chat_room_id
-      params[:id]
-    end
-
-    def page
-      params[:page]
-    end
-
-
-    def list_chat_rooms
+    
+    def list_chat_rooms(inputs)
       current_user.chat_rooms
                   .order(created_at: :desc)
-                  .page(page)
+                  .page(inputs[:page])
                   .per(ChatRoom::MESSAGES_PER_PAGE)
     end
+    
+    def chat_room_params
+      params.require(:chat_room).permit(:name)
+      
+    end
+    
+    def show_inputs
+      p = params.permit(:id, :page)
+      
+      { id: p[:id], page: normalize_page(p[:page]) }
+    end
+    
+    def normalize_page(raw)
+      n = raw.to_i
+      [n, 1].max
+    end
+    
 end
